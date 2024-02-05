ab97ffb234fbbbb6dfa0bf9a4af20c72ff1d61bf
mramzannasir <mramzannasir555@gmail.com>
2024-02-05 17:13:21 +0500

    all done without footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px',
+        "2xl": '1920px',
+        "3xl": '2500px',
+      },
+      colors: {
+        'black': '#333333',
+        "gray": "#666666",
+        "brown": "#B88E2F"
+      },
+
+    },
   },
-  plugins: [],
+  "plugins": ["prettier-plugin-tailwindcss"]
 }
 
